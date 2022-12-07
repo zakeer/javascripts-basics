698944604e85b32bf7df0b6c770e61ef6994b14c
Syed Zakeer Hussain <syed.zakeer.hussain@sap.com>
2022-12-07 10:09:54 +0530

    javascript basics part one

---
+++ basics/strings.js
@@ -0,0 +1,17 @@
+"Hello World"
+'Hello World'
+`Hello World`
+
+"I am from \"Guntur\""
+
+'I am from \'Guntur'
+
+"Name: Zakeer City: Guntur Designation: UI Dev"
+
+`
+Name: Zakeer
+City: Guntur
+Designation: UI Dev
+`
+
+`5 plus 5 is ${5+5}`
