6d3539af216cb1bcb8d5bdd64f6e85aedbaccfaa
Zakeer Hussain <zakeer@zakeer.me>
2022-12-13 09:59:06 +0530

    let and const added

---
+++ 3-variables/2-let-and-const.js
@@ -0,0 +1,30 @@
+// before ES6 version, there is only one way to create 
+// variable in javasript is using var keyword
+var myVariable = "Some data";
+
+//in ES6 version, let and const are introduce to create
+// variables
+
+// var declaration with value
+var myName = "Syed";
+
+// var redeclaration with same variable identifier
+var myName = "Syed Zakeer";
+
+
+// let declaration with value
+let myHomeTown = "Guntur";
+
+// we can't redeclare let variable again
+// let myHomeTown = "ongole"; // Javascript will throw error
+
+// But we can reassign new value to var or let
+myName = "Syed Zakeer Hussain"
+myHomeTown = "Hyderabad"
+
+
+// for var and let we can reassign values (remove old value and update new value)
+
+// for const we need to declare and assign the value
+// we can't reassign const value
+const DOB = "29/01/1999";
