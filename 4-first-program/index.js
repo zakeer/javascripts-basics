07203bcabe09e6d084c136b6edfb216379e42fcc
Syed Zakeer Hussain <syed.zakeer.hussain@sap.com>
2022-12-15 10:04:50 +0530

    first program added

---
+++ 4-first-program/index.js
@@ -0,0 +1,29 @@
+var intial = "Syed";
+let firstName = "Zakeer";
+let lastName = "Hussain";
+
+const city = "Guntur";
+
+const friends = [
+  'Harish',
+  'Harsha',
+  'Mani'
+];
+
+const myDetails = {
+  fullname: `${firstName} ${lastName}, ${intial}`,
+  designation: 'Frontend Dev',
+  friends: friends,
+  city: city
+}
+
+console.log(myDetails);
+
+// Javascript Runtime Environment
+// 1. Browser
+// 2. Node
+
+// Run this file in NODE environment
+// example: node FILENAME
+// node index.js
+
