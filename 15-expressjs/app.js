9f02fe22574621a61194b748d7719c216123143d
Syed Zakeer Hussain <syed.zakeer.hussain@sap.com>
2023-02-06 10:09:39 +0530

    node express post method

--- 15-expressjs/app.js
+++ 15-expressjs/app.js
@@ -6,8 +6,14 @@
 
 const fs = require('fs');
 
-const express = require("express"); //
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+// Define app to use bodyParser middleware
+// Handling JSON payload requests
+app.use(bodyParser.json());
 
 function getFileData(path) {
   const data = fs.readFileSync(path);
@@ -22,12 +28,12 @@ const DATA_FOLDER = "../14-node-server/data"
 const products = getFileData(`${DATA_FOLDER}/products.json`);
 const categories = getFileData(`${DATA_FOLDER}/categories.json`);
 
-const handleCategories = function(request, response) {
+const handleCategories = function (request, response) {
   response.json(categories)
 };
 
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.send("Express app is runing...")
 });
 
@@ -41,7 +47,7 @@ app.get("/products/:productID", (req, res) => {
   const productId = req.params.productID;
   const product = products.find(p => p.id === parseInt(productId));
 
-  if(!product) {
+  if (!product) {
     res.json({
       error: true,
       message: `NO PRODUCT FOUND with ID ${productId}`
@@ -49,7 +55,45 @@ app.get("/products/:productID", (req, res) => {
   } else {
     res.json(product);
   }
-})
+});
+
+console.log([].length)
+
+/*
+app.post("/categories", (req, res) => {
+  const categoryName = `Category ` + categories.length;
+  categories.push(categoryName);
+
+  console.log("CHecking for Request Body");
+  console.log(req.body);
+
+  res.send({ success: true, newCategory: categoryName })
+});
+*/
+
+app.post("/add-category", (request, response) => {
+  const requestBody = request.body; // { "category": "NEW_CATEGORY_NAME" }
+  console.log("REquest body ", requestBody);
+  const category = requestBody.category;
+
+  if (category) {
+    // Inserting new user provided category to categories array 
+    categories.push(category);
+
+    response.send({
+      success: true,
+      message: `New ${category} added`,
+      count: categories.length
+    });
+  } else {
+    response.send({
+      success: false,
+      error: `Please provide category name`
+    });
+  }
+
+
+});
 
 app.listen(3000, () => {
   console.log(`Server listening on port http://localhost:3000`)
--- 15-expressjs/readme.md
+++ 15-expressjs/readme.md
@@ -7,6 +7,10 @@ Creating node app using [ExpressJS](https://expressjs.com/) framework
   `npm init` in project folder
 2. Install Express Framework
   `npm install express`
+3. Install Body Parser
+  `npm install body-parser`
+4. Apply body-parser middleware to express app for parsing json request body
+  `app.use(bodyParser.json())`
 
 ### App Routes
 - /
